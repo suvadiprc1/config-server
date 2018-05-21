eca26254a2026e5a9cf624a046a5c3f2b7332ff5
suvadip rc <suvadip.roychowdhury@ehi.com>
2018-05-21 12:18:19 +0530

    Config Server first commit.

---
+++ src/main/java/com/config/server/configserver/configs/KafkaProducerConfig.java
@@ -0,0 +1,19 @@
+package com.config.server.configserver.configs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+public class KafkaProducerConfig {
+	
+	/*public ProducerFactory<String, String> producerFactory() {
+		final Map<String, Object> configProps = new HashMap<String, Object>();
+		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, )
+				
+	}*/
+
+}
